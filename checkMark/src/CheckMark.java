0c44e9cc9d39103c86c5a4ffe09c41d09b607799
Power <zpower@sage.local>
2022-08-18 16:30:01 -0700

    check it

---
+++ checkMark/src/CheckMark.java
@@ -0,0 +1,16 @@
+import java.io.PrintWriter;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        String checkMark = new String("\u001B[32m" + "✅" + "\u001B[0m");
+
+        System.out.println("Constructor test " + checkMark);
+        System.out.println("ToString test " + checkMark);
+        System.out.println("A on coding test " + checkMark);
+        System.out.println("Life better now because of " + checkMark);
+
+
+    }
+}
