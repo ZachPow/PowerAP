7580a50ed52e0bb5e779134fa1412c2dad8182d5
Power <zpower@sage.local>
2022-08-24 10:59:53 -0700

    added comments to PA1

--- TempConverter/TempConverter.java
+++ TempConverter/TempConverter.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 public class TempConverter {
     public static void main(String args[]){
         
+        //variable declarations
         Scanner input = new Scanner(System.in);
-        int temp = 0;
+        int fahrenheit = 0;
 
+        //ask for input
         System.out.println("please enter a value in fahrenheit to be converted to kelvin");
         
-        temp = input.nextInt();
+        //get input
+        fahrenheit = input.nextInt();
 
-        System.out.println("your value in Kelvin is " + kelvinToFahrenheit(temp));
+        //print result of function
+        System.out.println("your value in Kelvin is " + fahrenheitToKelvin(fahrenheit));
 
     }
     public static float fahrenheitToKelvin(int val){
 
+        //return the conversion of farenheit to Kelvin
         return (float)((val-32) * 5.0/9 + 273.15);
     }
     public static float kelvinToFahrenheit (int val){
+        
+        //return the conversion of kelvin to fhrenheit
         return (float)((val-273.15) * 9.0/5 + 32);
     }
 }
--- TimeConverter/TimeConverter.java
+++ TimeConverter/TimeConverter.java
@@ -3,36 +3,46 @@ import java.util.Scanner;
 public class TimeConverter {
     public static void main(String args[]){
 
+        //variable declarations
         Scanner input = new Scanner(System.in);
         int seconds = 0;
 
+        //ask for input
         System.out.println("Input a amount of time in seconds");
 
+        //get input
         seconds = input.nextInt();
 
+        //call function
         printTimeConversion(seconds);
         
     }
     public static void printTimeConversion(int seconds){
 
+        //variable declaration
         int minutes = 0;
         int hours = 0;
         int days = 0;
         int years = 0;
 
 
+        //convert seconds to minutes and find remainder
         minutes = seconds/60;
         seconds = seconds%60;
 
+        //convert minutes to hours and find remainder
         hours = minutes/60;
         minutes %= 60;
 
+        //convert hours to dyas and find remainder
         days = hours/24;
         hours %= 24;
 
+        //convert days to years and find remainder
         years = days/365;
         days %= 365;
         
+        //print out the time in terms of years, days, hours, minutes, and secondss
         System.out.println("years: " + years + " days: " + days + " hours " + hours + " minutes: " + minutes + " seconds " + seconds);
 
     }
