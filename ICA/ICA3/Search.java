91685096dd64145dbd8a7f133486e5937c1f634b
Power <zpower@sage.local>
2022-11-01 10:19:41 -0700

    ICA3 updated

--- ICA/ICA3/Search.java
+++ ICA/ICA3/Search.java
@@ -5,52 +5,43 @@ public class Search {
         
         
         //variable declaration
-        int[] arr = new int[20];
+        int[] arr = new int[10000];
         int comparisons = 0;
         int target = 0;
-        int index = -1;
-        int minIndex = 0;
-        int maxIndex = arr.length-1;
-        int midIndex = 0;
+ 
 
         Scanner input = new Scanner(System.in);
 
         //asign each index to random value
         for(int i = 0; i < arr.length; i++){
-            arr[i] = (int)(Math.random()*10);
+            arr[i] = (int)(Math.random()*20000);
             
         }
 
         System.out.println("Please enter value to search for: ");
+
         //get target value from user
         target = input.nextInt();
 
+        System.out.println("Max: " + getMax(arr));
+        System.out.println("Min: " + getMin(arr));
+
         //sort array
         selectionSort(arr);
-        printArray(arr);
+        //printArray(arr);
         
         System.out.println('\n' + "Target: " + target + '\n');
 
         //start timer for linear search
         long start = System.nanoTime();
         
-        //linear search
-        //increments both i and comparisons
-        //checks if arr[i] == target if it does break out of the loop
-        for(int i = 0; i < arr.length; i++, comparisons++){
-            if(arr[i] == target){
-                index = i;
-                break;
-            }
-        }
+        comparisons = linearSearch(arr, target);
 
-        //stop timer for linear search
         long end = System.nanoTime();
 
         
         //print out data related to the linear search
         System.out.println("Comparisons in Linear Search: " + comparisons);
-        System.out.println("Index: " + index);
         System.out.println("Time: " + (end-start) + " nanoseconds - " + (end-start)*0.000001 + " miliseconds");
 
         //divider between Linear and Binary data
@@ -59,43 +50,41 @@ public class Search {
 
         //reset variables so they can be used in binary search
         comparisons = 0;
-        index = -1;
 
         //start timer for binary search
         start = System.nanoTime();
 
         //binary search
         //conitues the loop while the minIndex and MaxIndex dont overlap
-        while(minIndex < maxIndex){
-            
-            //assign the mid index
-            midIndex = (maxIndex+minIndex)/2;
-            //increment the comparisons
-            comparisons++;
-            
-            //check if we found the target
-            if(arr[midIndex] == target){
-                index = midIndex;
-                break;
-            
-            //moves MinIndex to MidIndex+1 if target is bigger than the midIndex
-            }else if(arr[midIndex] < target){
-                minIndex = midIndex+1;
+        comparisons = binarySearch(arr, target);
 
-            //moves MaxIndex to MidIndex-1 if target is smaller than the midIndex
-            }else if(arr[midIndex] > target){
-                maxIndex = midIndex-1;
-            }
-
-        }
         //stops binary search timer
         end = System.nanoTime();
 
         //prints out data related to binary search
         System.out.println("Comparisons in binary search: " + comparisons);
-        System.out.println("Index: " + index);
         System.out.println("Time: " + (end-start) + " nanoseconds - " + (end-start)*0.000001 + " miliseconds");
         
+    }
+    public static int getMax(int[] arr){
+        int max = arr[0];
+
+        for(int i : arr){
+            if(max < i){
+                max = i;
+            }
+        }
+        return max;
+    }
+    public static int getMin(int[] arr){
+        int min = arr[0];
+
+        for(int i : arr){
+            if(min > i){
+                min = i;
+            }
+        }
+        return min;
     }
     public static void printArray(int[] arr){
         for(int i = 0; i < arr.length; i++){
@@ -126,16 +115,31 @@ public class Search {
 
         }
     }
+    public static int linearSearch(int[] arr, int target){
+
+        int comparisons = 0;
+        for(int i = 0; i < arr.length; i++, comparisons++){
+            if(arr[i] == target){
+                return comparisons;
+            }
+        }
+        return comparisons;
+
+    }
     public static int binarySearch(int[] arr, int target){
         int minIndex = 0;
         int maxIndex = arr.length-1;
         int index = 0;
-
+        int comparisons = 0;
+        
         while(minIndex < maxIndex){
+
             int midIndex = (maxIndex+minIndex)/2;
+
+            comparisons++;
             
             if(arr[midIndex] == target){
-                return midIndex;
+                return comparisons;
             }else if(arr[midIndex] < target){
                 minIndex = midIndex+1;
             }else if(arr[midIndex] > target){
@@ -143,6 +147,6 @@ public class Search {
             }
 
         }
-        return -1;
+        return comparisons;
     }
 }
