c5b7c5d93f00ba820b82371b6e55b19fcd049938
Power <zpower@sage.local>
2022-11-28 12:05:12 -0800

    PA4

--- PA/PA4/PA4.java
+++ PA/PA4/PA4.java
@@ -4,10 +4,10 @@ public class PA4 {
     public static void main(String args[]){
 
         int attributePoints = 20;
-        int armor;
-        int health;
-        int magicArmor;
-        String temp;
+        int armor = 0;
+        int health = 0;
+        int magicArmor = 0;
+        int temp;
 
         Scanner input = new Scanner(System.in);
         Player oponent = new Player(attributePoints);
@@ -15,54 +15,85 @@ public class PA4 {
 
         System.out.println("Stat Points: " + attributePoints);
         System.out.println("How many points would you like to put into Health");
-        temp = input.next();
-        
-        if(!validInput(temp, attributePoints)){
-            System.out.println("bad input");
-            return;
-        }
-        health = Integer.parseInt(temp);
-        attributePoints -= health;
+
+        temp = getNumberInput(attributePoints);
+        health += temp;
+        attributePoints -= temp;
 
         System.out.println("Stat Points: " + attributePoints);
-        System.out.println("How many points would you like to put into Armor");
-        temp = input.next();
-        
-        if(!validInput(temp, attributePoints)){
-            System.out.println("bad input");
-            return;
-        }
-        armor = Integer.parseInt(temp);
-        attributePoints -= armor;
+        System.out.println("How many points would you like to put into armor");
+
+        temp = getNumberInput(attributePoints);
+        armor += temp;
+        attributePoints -= temp;
 
         System.out.println("Stat Points: " + attributePoints);
         System.out.println("How many points would you like to put into magic armor");
-        temp = input.next();
-        
-        if(!validInput(temp, attributePoints)){
-            System.out.println("bad input");
-            return;
-        }
-        magicArmor = Integer.parseInt(temp);
-        attributePoints -= armor;
-        
-        
+
+        temp = getNumberInput(attributePoints);
+        magicArmor += temp;
+        attributePoints -= temp;
+
+        Player player = new Player(health, armor, magicArmor, attributePoints); 
+    
+        System.out.println("Player: " + player);
+        System.out.println("Oponent: " + oponent);
     }
+    public static String getAttackStatusInput(){
+        
+        Scanner input = new Scanner(System.in);
+        String str = new String();
+
+        str = input.next();
+
+        while(!Player.validAttackState(str)){
+            str = input.next();
+        }
+
+        return str;
+    }
+    //gets input from user
+    public static int getNumberInput(int attributePoints){
+
+        Scanner input = new Scanner(System.in);
+        String val;
+        
+        //get input
+        val = input.next();
+        
+        //check if we got a valid input
+        //propmt the user for a new input if it is invalid
+        while(!validInput(val, attributePoints)){
+            System.out.println("bad input please give another input");
+            val = input.next();
+        }
+
+        //returns the string parsed to an integer
+        return Integer.parseInt(val);
+    }
+
+    //check that our input is valid
     public static boolean validInput(String s, int attributePoints){
         int temp;
-        
+
+        //attempt to parse input
+        //if we fail to parse the input return false
         try{
+
             temp = Integer.parseInt(s);
+
         }catch(Exception e){
-            System.out.println(e.getStackTrace());
+
+            //System.out.println(e.getStackTrace());
             return false;
         }
 
-        if(temp > attributePoints){
+        //return false if input is bigger than attribute points
+        //or if input is less than zero
+        if(temp > attributePoints || temp < 0){
             return false;
         }
         
         return true;
-
     }
 }
--- PA/PA4/Player.java
+++ PA/PA4/Player.java
@@ -5,19 +5,25 @@ public class Player{
     private int armor;
     private int magicArmor;
     private int attributePoints;
+    private String attackState;
 
+    //constructor used for player
     public Player(int healthIn, int armorIn, int magicArmorIn, int pointsIn){
         health = healthIn;
         armor = armorIn;
         magicArmor = magicArmorIn;
         attributePoints = pointsIn;
+        attackState = "none";
     }
+
+    //constructor used for computer
+    //assigns attribute to a random attribute until no attribute points are left 
     public Player(int attributePointsIn){
         int temp;
 
         attributePoints = attributePointsIn;
 
-        for(int i = attributePoints; i > 0; i--, attributePoints--){
+        while(attributePoints > 0){
             temp = (int)(Math.random()*3);
 
             switch(temp){
@@ -28,11 +34,13 @@ public class Player{
                 case 2: magicArmor++;
                     break;
                 default:
-                    System.out.println("initialization of player no work ");
+                    System.out.println("initialization of oponent no work ");
                     break;
             }
+            attributePoints--;
         }
     }
+    
     public String toString(){
 
         String temp = new String();
@@ -44,17 +52,77 @@ public class Player{
 
         return temp;
     }
+
     public int getHealth(){
         return health;
     }
-    public void subHealth(){
-        health--;
+    public void setAttackState(String s){
+        attackState = s;
     }
     public int getArmor(){
         return armor;
     }
     public int getMagicArmor(){
         return magicArmor;
+    }
+
+    public void addHealth(int value){
+        health += value;
+    }
+    public void subHealth(int value){
+        health -= value;
+    }
+
+    public static void attackRound(Player player, Player oponent){
+
+        //switch statement to evaluate players attackState
+        switch(player.attackState){
+            case "heal":  player.addHealth(1);
+                          break;
+
+            case "spell": if(oponent.attackState.equals("defend"))
+                            oponent.subHealth(1);
+                          else if(oponent.attackState.equals("heal"))
+                            oponent.subHealth(2);
+                          break;
+
+            case "attack": if(oponent.attackState.equals("spell"))
+                                oponent.subHealth(1);
+                           else if(oponent.attackState.equals("heal"))
+                                oponent.subHealth(2);
+                           break;
+        }
+
+        //switch statement to evaluate oponents attackStatus
+        switch(oponent.attackState){
+            case "heal":  oponent.addHealth(1);
+                          break;
+
+            case "spell": if(player.attackState.equals("defend"))
+                            player.subHealth(1);
+                          else if(player.attackState.equals("heal"))
+                            player.subHealth(2);
+                          break;
+
+            case "attack": if(player.attackState.equals("spell"))
+                                player.subHealth(1);
+                           else if(player.attackState.equals("heal"))
+                                player.subHealth(2);
+                           break;
+        }
+
+    }
+
+    public static boolean validAttackState(String attackStatus){
+        
+        if(attackStatus.equals("attack") 
+                || attackStatus.equals("defend")
+                || attackStatus.equals("spell")
+                || attackStatus.equals("heal")){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
