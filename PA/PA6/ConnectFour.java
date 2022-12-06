ff81879b11995bbbee62edcca52c2ec19c7cc0dd
ZachPow <zpower@sageridge.org>
2022-12-05 17:05:12 -0800

    connect four

--- PA/PA6/ConnectFour.java
+++ PA/PA6/ConnectFour.java
@@ -1,23 +1,76 @@
+import java.util.Scanner;
+
 public class ConnectFour{
     public static void main(String args[]){
         int[][]board = new int[7][7];
 
+        Scanner input = new Scanner(System.in);
+        String temp;
+        int[] pos;
+        int i = 0;
+
+        do{
+            printBoard(board);
+            temp = input.next();
+
+            if(i % 2 == 0){
+                pos = placeToken(board, Integer.parseInt(temp), 1);
+            }else{
+                pos = placeToken(board, Integer.parseInt(temp), 2);
+            }
+            i++;
+
+        }while(!checkForWin(board, pos[0], pos[1]));
+
+        System.out.println("X: " + pos[0] + " Y: " + pos[1]);
+
         printBoard(board);
 
-        placeToken(board, 1, 2);
-        placeToken(board, 1, 2);
-        placeToken(board, 1, 2);
-        placeToken(board, 1, 1);
-        placeToken(board, 1, 1);
-        placeToken(board, 1, 1);
+        if(checkForWin(board, pos[0], pos[1])){
+            System.out.println("yay");
+            System.out.println(board[pos[0]][pos[1]] + "won");
+        } 
+
+        // /System.out.println("row found");
         
-        printBoard(board);
+    }
+     //looks to see if a row or a streak of 4 can be found
+    //it checks to see how many tokens are to the right
+    //and checks how many tokens are to the left
+    //based on the token passed in which is dictated by row and columb arguments
+    public static boolean checkForWin(int[][] board, int row, int column){
 
-        if(checkForWin(board, 3, 1)){
-            System.out.println("row found");
-        }else{
-            System.out.println("no row found");
+        int count = 1;
+        
+        int x = column;
+        int y = row;
+
+        //what type of token are we looking for
+        int tokenType = board[y][x];
+
+        int rows = board.length;
+        int columns = board[0].length;
+
+        if(checkForRow(board, column, row, columns, rows, tokenType)){
+            System.out.println("row");
+            return true;
         }
+        if(checkForVerticalRow(board, column, row, columns, rows, tokenType)){
+            System.out.println("vertical");
+            return true;
+        }
+        if(checkForForwardSlah(board, column, row, columns, rows, tokenType)){
+            System.out.println("forwardSlash");
+            return true;
+        }
+        if(checkForSlash(board, column, row, columns, rows, tokenType)){
+            System.out.println("backSlash");
+            return true;
+        }else{
+            //System.out.println("no slash");
+        }
+        return false;
+        
     }
     public static boolean checkForForwardSlah(int[][] board, int column, 
                                                 int row, int columns, 
@@ -27,7 +80,26 @@ public class ConnectFour{
         int y = row;
         int count = 1;
 
-        if(y + 1 < row && x-1 >= 0){
+        if(y-1>= 0 && x-1 >= 0){
+            y--;
+            x--;
+        }
+        while(board[y][x] == tokenType){
+            
+            count++;
+
+            if(y-1>= 0 && x-1 >= 0){
+                y--;
+                x--;
+            }else{
+                break;
+            }
+        }
+        
+        y = column;
+        x = row;
+
+        if(y+1 < row && x+1 < column){
             y++;
             x++;
         }
@@ -35,30 +107,15 @@ public class ConnectFour{
             
             count++;
 
-            if(y + 1 < row && x+1 < column ){
+            if(y+1 < row && x+1 < column){
                 y++;
-                x--;
-            }else{
-                break;
-            }
-        }
-        if(y-1 >= 0 && x+1 < column){
-            y--;
-            x++;
-        }
-        while(board[y][x] == tokenType){
-            
-            count++;
-
-            if(y-1 >= 0 && x+1 < column){
-                y--;
                 x++;
             }else{
                 break;
             }
         }
+
         if(count >= 4){
-            System.out.println("forward slant row");
             return true;
         }else{
             return false;
@@ -72,10 +129,12 @@ public class ConnectFour{
         int y = row;
         int count = 1;
 
-        if(y + 1 < row && x+1 < column ){
-            y++;
+        
+        if(y - 1 >= rows && x+1 < columns ){
+            y--;
             x++;
         }
+        
         while(board[y][x] == tokenType){
             
             count++;
@@ -83,27 +142,32 @@ public class ConnectFour{
             if(y + 1 < row && x+1 < column ){
                 y++;
                 x++;
-            }{
+            }else{
                 break;
             }
         }
-        if(y-1 >= 0 && x-1 >= 0){
-            y--;
+
+        y = row;
+        x = column;
+        
+        System.out.println("count: " + count);
+        if(y+1 < rows && x-1 >= 0){
+            y++;
             x--;
         }
         while(board[y][x] == tokenType){
             
             count++;
 
-            if(y-1 >= 0 && x-1 >= 0){
-                y--;
+            if(y+1 < rows && x-1 >= 0){
+                y++;
                 x--;
             }else{
                 break;
             }
         }
+        
         if(count >= 4){
-            System.out.println("slant row");
             return true;
         }else{
             return false;
@@ -211,54 +275,28 @@ public class ConnectFour{
         }
 
     }
-    //looks to see if a row or a streak of 4 can be found
-    //it checks to see how many tokens are to the right
-    //and checks how many tokens are to the left
-    //based on the token passed in which is dictated by row and columb arguments
-    public static boolean checkForWin(int[][] board, int row, int column){
-
-        int count = 1;
-        
-        int x = column;
-        int y = row;
-
-        //what type of token are we looking for
-        int tokenType = board[y][x];
-
-        int rows = board.length;
-        int columns = board[0].length;
-
-        if(checkForRow(board, column, row, columns, rows, tokenType)){
-            return true;
-        }
-        if(checkForVerticalRow(board, column, row, columns, rows, tokenType)){
-            return true;
-        }
-        if(checkForForwardSlah(board, column, row, columns, rows, tokenType)){
-            return true;
-        }
-        if(checkForSlash(board, column, row, columns, rows, tokenType)){
-            return true;
-        }
-        return false;
-        
-    }
+   
     //places a token by checking each row in the column passed in
-    public static void placeToken(int[][] board, int column, int move){
+    public static int[] placeToken(int[][] board, int column, int move){
         
+        int[] arr = new int[2];
+
         //starts at the top row/board.length-1 and decrements
         //iterates until a token is found or we have reached the bottom row
         //if we find a row place token in the row above it
         for(int row = 0; row < board.length; row++){
             if(board[row][column] != 0){
                 board[row-1][column] = move;
-                System.out.println("placed: " + row + " " + column);
-                return;
+                arr[0] = row-1;
+                arr[1] = column;
+                return arr;
             }
         }
         //place token in bottom row
         board[board.length-1][column] = move;
-        System.out.println("placed bottom");
+        arr[0] = board.length-1;
+        arr[1] = column;
+        return arr;
     }
     public static void printBoard(int[][] board){
         for(int row = 0; row < board.length; row++){
